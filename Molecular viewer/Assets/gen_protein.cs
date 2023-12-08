5aa4f91aebe74f55c0300438a5f6b14737fba99a
VRAdministrator <127287625+VRAdministrator@users.noreply.github.com>
2023-12-08 04:43:52 -0700

    added basic helix structure

--- Molecular viewer/Assets/gen_protein.cs
+++ Molecular viewer/Assets/gen_protein.cs
@@ -199,7 +199,7 @@ public class gen_protein : MonoBehaviour
         List<string> eles=new List<string>();
         List<Vector3> atom_pos=new List<Vector3>();
         List<Vector3> amino_pos=new List<Vector3>();
-        int preres=0;
+        int preres=1;
         float div=0.0F;
         Vector3 cur_amino_pos=new Vector3();
         List<int> start_helix=new List<int>();
@@ -247,11 +247,12 @@ public class gen_protein : MonoBehaviour
                 total++;
                 start+=81;
             }else if (text.Substring(start,5)=="HELIX"){
-                int res_start=22;
+                int res_start=20;
                 for (;text[start+res_start]==" "[0];res_start++){}
                 start_helix.Add(int.Parse(text.Substring(start+res_start,26-res_start))-1);
-                res_start=34;
-                end_helix.Add(int.Parse(text.Substring(start+res_start,26-res_start))-1);
+                res_start=32;
+                for (;text[start+res_start]==" "[0];res_start++){}
+                end_helix.Add(int.Parse(text.Substring(start+res_start,38-res_start))-1);
                 start+=81;
             }else{
                 for (;text[start]!=10;){
@@ -283,26 +284,27 @@ public class gen_protein : MonoBehaviour
         }
         int size=amino_pos.Count-1;
         Vector3 dif=new Vector3();
-        bones_obs=new List<GameObject>(new GameObject[size]);
+        bones_obs=new List<GameObject>();
         int pt=0;
         int len_helix=start_helix.Count;
         for (int i=0;i<size;i++){
-            if (len_helix<pt){
+            if (pt<len_helix){
                 if (start_helix[pt]==i){
                     Vector3 end_vt=amino_pos[end_helix[pt]];
                     Vector3 start_vt=amino_pos[start_helix[pt]];
-                    int turns=(int)(Mathf.Ceil((float)(start_helix[pt]-end_helix[pt])/3.5F))+1;
+                    int turns=(int)(Mathf.Ceil((float)(end_helix[pt]-start_helix[pt])/3.5F))+1;
                     Vector3 offset=(end_vt-start_vt)/turns;
                     start_vt+=offset/2;
                     for (int I=1;I<turns;I++){
                         GameObject helix=Instantiate(helix_model,trans);
                         start_vt+=offset;
                         helix.transform.position=start_vt;
+                        helix.transform.LookAt(amino_pos[end_helix[pt]]);
+                        bones_obs.Add(helix);
+                        helix.GetComponent<Renderer>().enabled=false;
                     }
-
                     i=end_helix[pt];
                     pt++;
-                    continue;
                 }
             }
             GameObject bone=Instantiate(bond_model,trans);
@@ -311,7 +313,7 @@ public class gen_protein : MonoBehaviour
             bone.transform.position=(amino_pos[i]+amino_pos[i+1])/2;
             bone.transform.LookAt(amino_pos[i]);
             bone.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
-            bones_obs[i]=bone;
+            bones_obs.Add(bone);
             bone.GetComponent<Renderer>().enabled=false;
         }
         int spacer=Cs.Count()/100;
